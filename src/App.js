7ebdf4bd68c668940c38b991e20ae2b649b6d9ad
CindyBarria <cindybarria@gmail.com>
2020-08-24 13:24:11 -0400

    Avances de cover y ent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+//<---------Librerias-------->
 import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
+//<---------CSS-------------->
 import './App.css';
-import Cover from './componets/cover';
-import Ent from './componets/ent';
+//<---------Components------->
+import Cover from './componets/Cover';
+import Ent from './componets/Ent';
 import Game from './componets/game';
 import Home from './componets/home';
 import LoginModal from './componets/loginModal';
@@ -17,24 +19,15 @@ import Ranking from './componets/ranking';
 const App = () => {
   return (
     <Router>
-      <div>
-        <Link to="/">Portada</Link>
-        <Link to="/ent">Inicio</Link>
-        <Link to="/home">Home</Link>
-        <Link to="/game">Juegos</Link>
-        <Link to="/modules">Modulos</Link>
-        <Link to="/loginModal">Login del Juego</Link>
-        <Link to="/ranking">Ranking</Link>       
-        <Switch>
-          <Route path="/" exact component= {Cover}/>
-          <Route path="/ent" component= {Ent}/>
-          <Route path="/game" component= {Game}/>  
-          <Route path="/home" component= {Home}/>
-          <Route path="/loginModal" component= {LoginModal}/>
-          <Route path="/modules" component= {Modules}/>
-          <Route path="/ranking" component= {Ranking}/>             
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/" exact component={Cover} />
+        <Route path="/ent" component={Ent} />
+        <Route path="/game" component={Game} />
+        <Route path="/home" component={Home} />
+        <Route path="/loginModal" component={LoginModal} />
+        <Route path="/modules" component={Modules} />
+        <Route path="/ranking" component={Ranking} />
+      </Switch>
     </Router>
   );
 }
